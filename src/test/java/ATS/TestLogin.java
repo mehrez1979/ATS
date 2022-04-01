74225d319c08e5d067d8b8f9ece311f5d1d0a8dc
MEHREZ <MEHREZ@DESKTOP-ACK9F1N>
2022-04-01 02:23:29 +0200

    hi

---
+++ src/test/java/ATS/TestLogin.java
@@ -0,0 +1,24 @@
+package ATS;
+
+import org.testng.annotations.Test;
+
+import com.xpandit.testng.annotations.Xray;
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class TestLogin {
+    @Test
+    @Xray(requirement = "HTUTO-34", test="HTUTO-35")
+  public void f() {
+    	System.out.println("jjj");
+  }
+  @BeforeSuite
+  public void beforeSuite() {
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+  }
+
+}
