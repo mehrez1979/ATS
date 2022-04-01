363caa93c611c37eb36708b543b16b0dfe88cceb
MEHREZ <MEHREZ@DESKTOP-ACK9F1N>
2022-04-01 02:30:21 +0200

    d

--- src/test/java/ATS/TestLogin.java
+++ src/test/java/ATS/TestLogin.java
@@ -11,7 +11,7 @@ public class TestLogin {
     @Test
     @Xray(requirement = "HTUTO-34", test="HTUTO-35")
   public void f() {
-    	System.out.println("jjjd);
+    	System.out.println("jjjd");
   }
   @BeforeSuite
   public void beforeSuite() {
